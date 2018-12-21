800c8e61d968ae0ece997f1a695441b1bb7e54c1
matthenge <matthenge@gmail.com>
2018-12-21 14:45:16 +0300

    [feature #162790651]Added login models

--- app/api/v1/models/user_models.py
+++ app/api/v1/models/user_models.py
@@ -1,19 +1,30 @@
 """User models"""
+from app.api.v1.utils.manage import find_username, find_password
 users = []
+
 
 class UserModels:
     """User models class"""
     def __init__(self):
         """Initialize the user model class"""
-        self.db = users
+        pass
 
-    def add_user(self, username, email, password):
+    def add_user(self, username, email, password, confirm_password):
         """Adding new users"""
         user_data = {
-            'user_id' : len(users) + 1,
-            'username' : username,
-            'email' : email,
-            'password' : password      
+            'user_id': len(users) + 1,
+            'username': username,
+            'email': email,
+            'password': password,
+            'confirm_password': confirm_password
         }
-        user_details = self.db.append(user_data)
+        user_details = users.append(user_data)
         return user_details
+
+    def find_by_username(self, username):
+        """Find a user by username"""
+        return find_username(users, username)
+
+    def check_password(self, password):
+        """Check if password matches"""
+        return find_password(users, password)
