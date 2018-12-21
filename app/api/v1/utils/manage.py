ded602c1d67aa5c44b1bb2f45800418499caf5f4
matthenge <matthenge@gmail.com>
2018-12-21 14:44:02 +0300

    [feature #162790651]Added functions to fetch username and password

--- app/api/v1/utils/manage.py
+++ app/api/v1/utils/manage.py
@@ -6,3 +6,17 @@ def fetch_one(questions, questionId):
     for question in questions:
             if int(questionId) == question['questionId']:
                 return question
+
+
+def find_username(users, username):
+    """Find the username in the database"""
+    for user in users:
+        if username == user['username']:
+            return user
+
+
+def find_password(users, password):
+    """Look if the password matches the one in the database"""
+    for user in users:
+        if password == user['password']:
+            return user
