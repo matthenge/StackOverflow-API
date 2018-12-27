f3e49f766684f3421ad3aa84992e43933ec6b577
matthenge <matthenge@gmail.com>
2018-12-27 13:28:22 +0300

    [feature #162824537]Add password hashing methods

--- app/api/v1/utils/manage.py
+++ app/api/v1/utils/manage.py
@@ -1,4 +1,5 @@
 """Resource for shared functions"""
+import hashlib
 
 
 def fetch_one(questions, questionId):
@@ -13,3 +14,18 @@ def find_username(users, username):
     for user in users:
         if username == user['username']:
             return user
+
+
+def hash_password(password, username):
+    """Hash user passwords"""
+    salt = password + username
+    hashed = hashlib.md5(str.encode(salt)).hexdigest()
+    return hashed
+
+
+def check_hash_password(password, hashed):
+    """Check hashed passwords are same"""
+    if password == hashed:
+        return True
+    else:
+        return False
