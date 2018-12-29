9568769da0d169e1a9c40bc5784e4bcbc7ec01fb
matthenge <matthenge@gmail.com>
2018-12-29 21:46:23 +0300

    [feature #162876010]Add configuration to environments

--- config.py
+++ config.py
@@ -1,26 +1,29 @@
 import os
 
+
 class Config():
     """The main configuration class"""
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.getenv("SECRET_KEY")
-    
+
 
 class DevtConfiguration(Config):
     """The development configuration class"""
     DEBUG = True
     TESTING = True
 
+
 class ProdConfiguration(Config):
     """The production configuration"""
     DEBUG = False
-    TESTING = False  
+    TESTING = False
+
 
 class TestConfiguration(Config):
     DEBUG = True
     TESTING = True
-    
+
 configuration = {
     "devt": DevtConfiguration,
     "prod": ProdConfiguration,
