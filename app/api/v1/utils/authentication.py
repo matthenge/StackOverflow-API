843066d2ba089c3f96a69c9340ceec0a1822be88
matthenge <matthenge@gmail.com>
2018-12-29 21:43:05 +0300

    [feature #162876010]Add a decorator function to the authentication file

---
+++ app/api/v1/utils/authentication.py
@@ -0,0 +1,23 @@
+"""User authentication"""
+from flask import request, current_app
+import datetime
+import jwt
+from functools import wraps
+
+
+def login_required(f):
+    @wraps(f)
+    def authenticate(*args, **kwargs):
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+            try:
+                data = jwt.decode(token, current_app.config['SECRET_KEY'],
+                                  algorithm='HS256')
+            except jwt.ExpiredSignatureError:
+                return 'expired token'
+            except jwt.InvalidTokenError:
+                return 'invalid token'
+            return f(*args, **kwargs, username=data['username'])
+        else:
+            return 'missing token'
+    return authenticate
